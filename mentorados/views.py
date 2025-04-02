c7df88eff64666e633bf98929b64acc1cf4e672f
KK47Force <klebersonksp10@gmail.com>
2025-04-01 22:51:53 -0300

    adicionadas funcionalidades de cadastro de mentorados com validação de usuário, exibição de mensagens de sucesso e dados do usuario aparecendo no html

--- mentorados/views.py
+++ mentorados/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Mentorados, Navigators
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
 def mentorados(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+
     if request.method == 'GET':
-        return render(request, 'mentorados.html')
+        navegators = Navigators.objects.filter(user=request.user)
+        mentorados = Mentorados.objects.filter(user=request.user).order_by('-criado_em')
+        return render(request, 'mentorados.html', {'estagios': Mentorados.estagio_choices, 
+                                                   'navegators': navegators,
+                                                   'mentorados': mentorados,})
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        estagio = request.POST.get('estagio')
+        navigator = request.POST.get('navigator')
+        foto = request.FILES.get('foto')
+
+        mentorado = Mentorados(
+            nome=nome,
+            estagio=estagio,
+            navigator_id=navigator,
+            foto=foto,
+            user=request.user
+        )
+        mentorado.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Mentorado cadastrado com sucesso!')
+        return redirect('mentorados')
